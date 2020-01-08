2e14a05e70fa425a05d6b555b533dccbe178a630
Chris Rowett <rowett@yahoo.com>
2020-01-08 14:46:08 +0000

    fix Identify for 2-state RuleTable rules

--- js/lifeview.js
+++ js/lifeview.js
@@ -268,7 +268,7 @@
 		/** @const {string} */ versionName : "LifeViewer",
 
 		// build version
-		/** @const {number} */ versionBuild : 487,
+		/** @const {number} */ versionBuild : 488,
 
 		// author
 		/** @const {string} */ versionAuthor : "Chris Rowett",
