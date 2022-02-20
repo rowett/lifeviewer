5ad6c7052e87d302d57890a4fcd6b0ff621f1994
Chris Rowett <rowett@yahoo.com>
2022-02-20 07:41:01 +0000

    test for 701

--- js/lifeview.js
+++ js/lifeview.js
@@ -297,7 +297,7 @@
 		/** @const {string} */ screenShotTitle : "LifeViewer Image",
 
 		// build version
-		/** @const {number} */ versionBuild : 700,
+		/** @const {number} */ versionBuild : 701,
 
 		// author
 		/** @const {string} */ versionAuthor : "Chris Rowett",
