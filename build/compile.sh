49ab470c145886a132b39048dca54abd82b0a612
Chris Rowett <rowett@yahoo.com>
2024-02-19 22:36:13 +0000

    added build file for MacOS/Linux (thanks Tom!)

---
+++ build/compile.sh
@@ -0,0 +1,2 @@
+cat start.txt ../js/compatibility.js ../js/allocator.js ../js/alias.js ../js/canvasmenu.js ../js/patterns.js ../js/colours.js ../js/box.js ../js/snapshot.js ../js/hrot.js ../js/life.js ../js/script.js ../js/waypoint.js ../js/window.js ../js/random.js ../js/stars.js ../js/parser.js ../js/lifeview.js ../js/help.js ../js/keywords.js ../js/keys.js end.txt > all.js
+java -jar compiler.jar -W VERBOSE --jscomp_off=checkVars --compilation_level ADVANCED_OPTIMIZATIONS --js=all.js --js_output_file=lv-plugin.js
